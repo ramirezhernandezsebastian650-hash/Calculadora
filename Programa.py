dbba53abb3eb93187b9889a016c5eabf9d90a34a
Johan Enrique Soto Martinez <sotojohan006@gmail.com>
2026-08-27 17:21:05 -0500

    Añadir metodo mostrar menú

---
+++ Programa.py
@@ -0,0 +1,11 @@
+def mostrar_menu():
+    print("Seleccione una opción:")
+    print("1. Sumar")
+    print("2. Restar")
+    print("3. Multiplicar")
+    print("4. Dividir")
+    print("5. Salir")
+
+def sumar(a, b):
+    return a + b 
+print("Suma:", sumar(5, 3))
