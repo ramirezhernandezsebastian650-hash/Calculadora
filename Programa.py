481224a8ea3f44f0d6550fed5d582f5218f8009d
Sebastian Ramirez Hernandez <ramirezhernandezsebastian650@gmail.com>
2026-08-27 18:42:09 -0500

    Se agrega la funcion de restar

--- Programa.py
+++ Programa.py
@@ -18,6 +18,10 @@ def ejecutar_menu():
             print(f"el resultado de la suma es:{resultado}")
         elif opcion == '2':
             a = 2
+            num1 = float(input("Ingrese el un número: "))
+            num2 = float(input("Ingrese el otro número: ")) 
+            resultado = num1 + num2
+            print(f"el resultado de la suma es:{resultado}")
         elif opcion == '3':
             num1 = float(input("Ingrese el un número: "))
             num2 = float(input("Ingrese el otro número: "))
